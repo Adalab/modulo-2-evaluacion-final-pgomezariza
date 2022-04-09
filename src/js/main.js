8d3e8814464ba8f3648f49c03490c139405eb7bc
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-04-09 13:18:46 +0200

    add listen input and filter on fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,34 @@
 'use strict';
 
-//Obtener los datos de HTML con los que voy a trabajar (ul, input usuaria)
+//01. Obtener los datos de HTML con los que voy a trabajar (ul, input usuaria)
+//02. Pintar los datos según el input de la usuaria
+//03. Agregar a favoritos y quitar de favoritos
+//04. Mover de la "Lista" -> a la columna de "Cócteles Favoritos"
+//05. Almacenamiento local (Local Storage)
 
 
-//1. Llevo a JS el ul para pintar los cocktails
+
+//1. Llevo a JS el ul para pintar los cocktails y lista 
 const cocktailList = document.querySelector(".js-cocktailList");
+const favList = document.querySelector(".js-favList")
 
-//2. Llevo a JS el input para escuhar lo que busca 
+//2. Llevo a JS el input para escuhar lo que busca la usuaria 
 const inputSearch = document. querySelector(".js-input");
 
+//3. Necesito salvar/guardar los cocktails. Pero las tengo que guardar en una variable ("cockList = data.drinks ") para acceder en todo mi código. Va a estar vacio en inicio y tendrá valor cuando yo haga FETCH.
+let cockList = [];
 
-//3. Obtener los datos del servidor 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+//4. Creo una función que escuche al input y lo filtre en FETCH
+function handleKeyupInput(event) {
+  event.preventDefault();
+  callFetch(searchDrinks);
+}
+search.addEventListener('keyup',handleKeyupInput);
+
+//5. Obtener los datos del servidor con fetch y que los pinte 
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrinks}")
 .then(response => response.jason())
 .then(data => {
-  cocktailList = data.drinks;
-})
+  cockList = data.drinks;
+});
 
-//4. Necesito salvar/guardar los cocktails. Pero las tengo que guardar en una variable para acceder en todo mi código y las guardo y seria asi "cocktailList = data.drinks ". Va a estar vacio y tendrá valor cuando yo haga FETCH.
-let cocktailList =[];
